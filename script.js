c4e03c4abdad7463ec6b324e4a8f3934e00700c5
Halls <109175578+HallsRaul@users.noreply.github.com>
2024-04-02 00:11:53 -0300

    alterações e criação de drawer

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var button = document.getElementById("drawer");
+    var button2 = document.getElementById("drawer2");
+    var menu = document.querySelector(".menu");
+  
+    button.addEventListener("click", function() {
+      if (menu.classList.contains("open")) {
+        menu.classList.remove("open");
+      } else {
+        menu.classList.add("open");
+      }
+    });
+    button2.addEventListener("click", function() {
+        if (menu.classList.contains("open")) {
+          menu.classList.remove("open");
+        } else {
+          menu.classList.add("open");
+        }
+      });
+  });
