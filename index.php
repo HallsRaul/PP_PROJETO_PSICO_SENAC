a8f4635159c7dc4eda97d9c64e4faf73d50d5fb9
Halls <109175578+HallsRaul@users.noreply.github.com>
2024-04-12 09:14:30 -0300

    alterações finais design site (menos os cards)

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous"><link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="style.css">
+<script src="script.js"></script>
+<title>Document</title>
+</head>
+<body class="indexbody">
+  <nav class="navbar navbar-expand-lg navbar-dark" style="background-image: linear-gradient(rgba(0, 0, 0, 0.130), rgba(0, 0, 0, 0.120), rgba(0, 0, 0, 0.00));";>
+    </button><a class="navbar-brand" href="#">La Vitale</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav";>
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Inicio<span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Conteúdos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Sobre</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+    <a href="main.html" class="titulo1"><h1>La Vitale</h1></a>
+    <h1 class="texto1" style="text-decoration: underline; text-decoration-thickness:calc(2px);";>Aprenda, se cuide e se conheça!</h1>
+
+    <div class="imagem_menu">
+    <a href="https://www.senacrs.com.br/unidade/84"><img src="img/senaclogo.png"></a>
+    </div>
+</body>
+</html>
+?>
---
+++ main.php
@@ -0,0 +1,81 @@
+<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous"><link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="style.css">
+<script src="script.js"></script>
+<title>Document</title>
+</head>
+<body class="mainbody">
+    <button id="drawer">
+      <div class="iconmenu"></div>
+      <div class="iconmenu"></div>
+      <div class="iconmenu"></div>
+    </button>
+    <div class="menu"> 
+        <button id="drawer2">
+          VOLTAR
+        </button>
+        <ul>
+        <a href="index.html"><li>Pagina inicial</li></a>
+        <a href=""><li>Informações</li></a>
+        <a href=""><li>Conteúdos</li></a>
+        <a href=""><li>Psicologia no cotidiano</li></a>
+        <a href=""><li>Dúvidas</li></a>
+        <a href=""><li>Sobre nós</li></a>
+      </ul>
+    </div>
+    <main>
+    <center><p class="texto2">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+      Libero ab saepe adipisci similique error voluptatum architecto perferendis fuga 
+      incidunt? Assumenda porro vitae blanditiis quia laudantium nihil dicta a ducimus tempora. 
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, sit odit, numquam delectus
+      sequi ducimus quisquam ea nesciunt accusamus repudiandae provident eum libero quaerat unde vel, 
+      hic recusandae. Illo, velit? Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+      Explicabo, architecto eligendi omnis officiis sed temporibus pariatur asperiores? 
+      Totam quas quasi vero qui eligendi, facere quisquam repudiandae quidem libero aliquid 
+      voluptatum? Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, ab rerum. 
+      Alias labore voluptatem odio, reprehenderit temporibus maxime aliquam. Assumenda quidem 
+      accusantium neque praesentium et cupiditate similique ad reiciendis cum?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+      Libero ab saepe adipisci similique error voluptatum architecto perferendis fuga 
+      incidunt? Assumenda porro vitae blanditiis quia laudantium nihil dicta a ducimus tempora. 
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, sit odit, numquam delectus
+      sequi ducimus quisquam ea nesciunt accusamus repudiandae provident eum libero quaerat unde vel, 
+      hic recusandae. Illo, velit? Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+      Explicabo, architecto eligendi omnis officiis sed temporibus pariatur asperiores? 
+      Totam quas quasi vero qui eligendi, facere quisquam repudiandae quidem libero aliquid 
+      voluptatum? Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, ab rerum. 
+      Alias labore voluptatem odio, reprehenderit temporibus maxime aliquam. Assumenda quidem 
+      accusantium neque praesentium et cupiditate similique ad reiciendis cum?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+      Libero ab saepe adipisci similique error voluptatum architecto perferendis fuga 
+      incidunt? Assumenda porro vitae blanditiis quia laudantium nihil dicta a ducimus tempora. 
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, sit odit, numquam delectus
+      sequi ducimus quisquam ea nesciunt accusamus repudiandae provident eum libero quaerat unde vel, 
+      hic recusandae. Illo, velit? Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+      Explicabo, architecto eligendi omnis officiis sed temporibus pariatur asperiores? 
+      Totam quas quasi vero qui eligendi, facere quisquam repudiandae quidem libero aliquid 
+      voluptatum? Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, ab rerum. 
+      Alias labore voluptatem odio, reprehenderit temporibus maxime aliquam. Assumenda quidem 
+      accusantium neque praesentium et cupiditate similique ad reiciendis cum?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+      Libero ab saepe adipisci similique error voluptatum architecto perferendis fuga 
+      incidunt? Assumenda porro vitae blanditiis quia laudantium nihil dicta a ducimus tempora. 
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, sit odit, numquam delectus
+      sequi ducimus quisquam ea nesciunt accusamus repudiandae provident eum libero quaerat unde vel, 
+      hic recusandae. Illo, velit? Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+      Explicabo, architecto eligendi omnis officiis sed temporibus pariatur asperiores? 
+      Totam quas quasi vero qui eligendi, facere quisquam repudiandae quidem libero aliquid 
+      voluptatum? Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, ab rerum. 
+      Alias labore voluptatem odio, reprehenderit temporibus maxime aliquam. Assumenda quidem 
+      accusantium neque praesentium et cupiditate similique ad reiciendis cum?
+    </p></center>
+  </main>
+</body>
+</html>
+?>
