e2baa6ef612e9b3f1b83d13e7bf6f750c4da9768
Halls <109175578+HallsRaul@users.noreply.github.com>
2024-04-19 08:46:41 -0300

    alterações de responsividade site

--- index.php
+++ index.php
@@ -5,15 +5,13 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous"><link rel="stylesheet" href="style.css">
-<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="style.css?t=<?php echo date("YmdHis").rand(1,9999999)?>"/>
 <script src="script.js"></script>
 <title>Document</title>
 </head>
 <body class="indexbody">
   <nav class="navbar navbar-expand-lg navbar-dark" style="background-image: linear-gradient(rgba(0, 0, 0, 0.130), rgba(0, 0, 0, 0.120), rgba(0, 0, 0, 0.00));";>
     </button><a class="navbar-brand" href="#">La Vitale</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav";>
       <ul class="navbar-nav">
@@ -29,10 +27,21 @@
       </ul>
     </div>
   </nav>
-
-    <a href="main.php" class="titulo1"><h1>La Vitale</h1></a>
+  <div class="container mt-5 mb-5">
+    <a href="main.php" class="titulo1" style="font-size: 100px;"><h1 style="font-size: 70px;">La Vitale</h1></a>
+    <br>
+    <br>
+    <br>
+    <br>  
     <h1 class="texto1" style="text-decoration: underline; text-decoration-thickness:calc(2px);";>Aprenda, se cuide e se conheça!</h1>
-
+    <center><h2 class="h2-titulo">
+    <br>
+    <br>
+    <br>
+   <b>La Vitale</b> é um site criado para ajudar e conscientizar as pessoas sobre os acessos à psicologia e seus conceitos, como pode ser fundamental,
+    segura e, em alguns casos, muito importante. A psicologia deve ser respeitada e assim <b>sempre</b> deve ser.
+  </h2></center>
+  </div>
     <div class="imagem_menu">
     <a href="https://www.senacrs.com.br/unidade/84"><img src="img/senaclogo.png"></a>
     </div>
